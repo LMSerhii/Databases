e2f27482b683266a1a455560f7bbe03076697b01
LMSerhii <Leonovserhii89@gmail.com>
2023-01-20 22:05:45 +0200

    readme

--- MySQL/main.py
+++ MySQL/main.py
@@ -2,6 +2,7 @@ import pymysql.cursors
 
 from config import host, user, password, db_name
 
+
 try:
     connection = pymysql.connect(
         host=host,
@@ -16,13 +17,18 @@ try:
     print("=" * 30)
 
     try:
-        with connection.cursor() as cursor:
+        # with connection.cursor() as cursor:
             # cursor.execute("SHOW DATABASES;")
             # print(cursor.fetchall())
 
             # cursor.execute("SHOW TABLES;")
             # print(cursor.fetchall())
-            
+
+            # drop_table = "DROP TABLE `test_db`;"
+            # cursor.execute(drop_table)
+
+        # with connection.cursor() as cursor:
+
             # create_table_query = """CREATE TABLE `customers`(
             #                         `id` int(11) AUTO_INCREMENT,
             #                         `name` varchar(32),
@@ -41,28 +47,41 @@ try:
             #                         );"""
             # cursor.execute(create_table_query)
             # print("Table created successfully")
-      
+
+        # with connection.cursor() as cursor:
+
             # insert_query = """INSERT INTO `customers` (name, password, email)
             #                   VALUES ('Serhii', 'qwerty', 'serhii@gmail.com');"""
             # cursor.execute(insert_query)
             # connection.commit()
 
+        # with connection.cursor() as cursor:
+
             # update_query = "UPDATE `customers` SET name = 'Oleg' WHERE id = 1;"
             # cursor.execute(update_query)
             # connection.commit()
 
+        # with connection.cursor() as cursor:
+
             # delete_query = "DELETE FROM `my_users` WHERE id = 3;"
             # cursor.execute(delete_query)
             # connection.commit()
-        
-            # drop_table = "DROP TABLE `customers`;"
-            # cursor.execute(drop_table)
-        
-            select_all_rows = "SELECT * FROM `customers`"
-            cursor.execute(select_all_rows)
-            rows = cursor.fetchall()
-            for row in rows:
-                print(row)
+
+        # with connection.cursor() as cursor:
+        #
+        #     select_all_rows = "SELECT * FROM `customers`"
+        #     cursor.execute(select_all_rows)
+        #     rows = cursor.fetchall()
+        #     for row in rows:
+        #         print(row)
+
+        # with connection.cursor() as cursor:
+        #
+        #     select_all_rows = "SELECT * FROM `users`"
+        #     cursor.execute(select_all_rows)
+        #     rows = cursor.fetchall()
+        #     for row in rows:
+        #         print(row)
 
     finally:
         connection.close()
--- MySQL/readme.md
+++ MySQL/readme.md
@@ -1,4 +1,135 @@
-if return exception, try install
-pip install cryptography
-or 
+# MySQL 
+
+> Install MySQL on ubuntu / debian
+---
+#### Если sudo не установлено
+```
+apt install sudo 
+```
+#### Качаем ссылку на установочный файл deb и устанавливаем его
+```commandline
+sudo wget https://dev.mysql.com/get/mysql-apt-config_0.8.24-1_all.deb
+```
+```commandline
+sudo dpkg -i mysql....deb
+```
+### Обновляем пакеты
+```commandline
+sudo apt update
+```
+### Устанавливаем mysql-server
+```commandline
+sudo apt install mysql-server
+```
+### Установка пакета безопасности
+```commandline
+sudo mysql_secure_installation
+```
+##### Заходим из под root
+```commandline
+sudo mysql 
+```
+#### Меняем пароль
+
+```
+ALTER USER `root`@`localhost` IDENTIFIED 
+WITH mysql_native_password BY 'your_password';
+
+```
+
+### Заходим в файл конфига и меняем ip для входа из определенного или любого ip (0.0.0.0)
+```
+sudo nano /etc/mysql/mysql.conf.d/mysqld.cnf
+```
+### Системные команды
+
++ sudo systemctl status mysql
+
++ sudo systemctl start mysql
+
++ sudo systemctl enable mysql
+
++ sudo systemctl stop mysql 
+
+
+### Удаление 
+
+```
+dpkg -l | grep -i mysql
+```
+```commandline
+sudo systemctl stop mysql
+```
+```commandline
+sudo apt purge mysql
+```
+``` commandline
+sudo rm -Rf /var/lib/mysql/
+sudo rm -Rf /var/log/mysql
+sudo rm -Rf /etc/mysql
+```
+
+### Переустановка
+
+```commandline
+sudo apt update
+sudo apt install -f
+sudo apt install mysql-server
+sudo apt dist-upgrade
+```
+
+### Вход 
+
+```commandline
+
+mysql -u root -p 
+
+```
+---
+> Work in MySQL
+#### Все базы данных
+```
+SHOW DATABASES;
+```
+#### Все таблицы
+```
+SHOW TABLES;
+```
+#### Создать базу данных
+
+```
+CREATE DATABASE db_name;
+```
+#### Использовать базу данных 
+
+```
+USE db_name;
+```
+#### Удалить базу данных
+
+```
+DROP DATABASE db_name;
+```
+#### Создать пользователя
+
+```
+CREATE USER `username`@`ip_address` 
+IDENTIFIED BY 'your_password';
+```
+#### Права суперпользователя ко всем таблицам данной базы данных
+ 
+```
+GRANT ALL PRIVILEGES ON db_name.* TO
+`username`@`ip_address`;
+```
+#### Показать всех пользователей
+
+```
+SELECT user, host FROM mysql.user;
+```
+---
+
+> Work in Python: 
+```
 python3 -m pip install PyMySQL[rsa] 
+```
